1ad6492c7fc5b7fe7038a4ddf3ad25adf897fa32
natbediee <natbediee@gmail.com>
2026-02-03 11:36:09 +0100

    test_ok

--- tp1/app/services.py
+++ tp1/app/services.py
@@ -1,10 +1,10 @@
 def compute_bonus(salary: float) -> float:
     if salary < 0:
         raise ValueError("Salary must be positive")
-    #if salary < 50000:
-     #   return salary * 0.05
-    #return salary * 0.10
+    if salary < 50000:
+        return salary * 0.05
+    return salary * 0.10
 
     # ERREUR VOLONTAIRE 
-    return salary * 0.2
+    #return salary * 0.2
 
