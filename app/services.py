8f2e5bcf08c53422d26dce9667ab8c11df2d9cda
natbediee <natbediee@gmail.com>
2026-04-02 09:48:06 +0200

    ERREUR BONUS

--- app/services.py
+++ app/services.py
@@ -1,9 +1,9 @@
 def compute_bonus(salary: float) -> float:
     if salary < 0:
         raise ValueError("Salary must be positive")
-    if salary < 50000:
-        return salary * 0.05
-    return salary * 0.10
+    #if salary < 50000:
+    #    return salary * 0.05
+    #return salary * 0.10
 
     # ERREUR VOLONTAIRE 
     return salary * 0.2
